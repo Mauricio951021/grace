4fa03b6d15a5b6f2308b8eff94494a52f67af893
Negan <mauriciovedado@gmail.com>
2026-04-03 23:27:15 +0200

    spawn

---
+++ src/spawn.rs
@@ -0,0 +1,57 @@
+
+
+pub(crate) fn spawn_root_task(fut: impl Future<Output = ()> + 'static) {
+    let task = Tarea(Box::into_raw(Box::new(Inner {
+        ref_counter: AtomicUsize::new(1),
+        id: TASK_ID.fetch_add(1, Relaxed),
+        state: AtomicU8::new(1),
+        future: UnsafeCell::new(Some(Box::pin(fut))),
+        ring: *CURRENT.get().unwrap(),
+        multi_thread: u8::MAX,
+    })));
+    TASK_COUNTER.fetch_add(1, Relaxed);
+    CURRENT.get().unwrap().inner().push_back(task);
+    CURRENT_ID.get().unwrap().unpark();
+}
+
+pub fn spawn_local(fut: impl Future<Output = ()> + 'static) {
+    assert!(thread::current().id() == CURRENT_ID.get().unwrap().id());
+    let task = Tarea(Box::into_raw(Box::new(Inner {
+        ref_counter: AtomicUsize::new(1),
+        id: TASK_ID.fetch_add(1, Relaxed),
+        state: AtomicU8::new(1),
+        future: UnsafeCell::new(Some(Box::pin(fut))),
+        ring: *CURRENT.get().unwrap(),
+        multi_thread: 0,
+    })));
+    TASK_COUNTER.fetch_add(1, Relaxed);
+    CURRENT.get().unwrap().inner().push_back(task);
+    CURRENT_ID.get().unwrap().unpark();
+}
+
+pub fn spawn(fut: impl Future<Output = ()> + Send + 'static) {
+    let task = Tarea(Box::into_raw(Box::new(Inner {
+        ref_counter: AtomicUsize::new(1),
+        id: TASK_ID.fetch_add(1, Relaxed),
+        state: AtomicU8::new(1),
+        future: UnsafeCell::new(Some(Box::pin(fut))),
+        ring: *MULTI.get().unwrap(),
+        multi_thread: 1,
+    })));
+    MULTI.get().unwrap().inner().push_back(task);
+    TASK_COUNTER.fetch_add(1, Relaxed);
+    let order = GLOBAL_COUNTER_FOR_ALTERNATIVE_WAKE.fetch_add(1, Relaxed);
+    let map = WORKERS_BITMAP.load(Relaxed);
+    if map == 0 {
+        return;
+    }
+    let idx = if (order & 1) == 0 {
+        map.trailing_zeros() as usize
+    } else {
+        (63 - map.leading_zeros()) as usize
+    };
+    WORKERS_BITMAP.fetch_and(!(1 << idx), Relaxed);
+    unsafe {
+        WORKERS_ID.get().unwrap()[idx].assume_init_ref().unpark();
+    }
+}
